1a86ccad742d2d965ea88af7aa5d62afde38faf5
ricar <ricarducorrea@gmail.com>
2022-02-21 14:00:32 -0300

    Adicao info Titulo de Eleitor

--- src/Dominio/TituloEleitor.java
+++ src/Dominio/TituloEleitor.java
@@ -9,6 +9,43 @@ public class TituloEleitor {
 	public Zona zona;
 	public Secao secao;
 	public boolean Status;
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	public Cidadao getCidadao() {
+		return cidadao;
+	}
+	public void setCidadao(Cidadao cidadao) {
+		this.cidadao = cidadao;
+	}
+	public Zona getZona() {
+		return zona;
+	}
+	public void setZona(Zona zona) {
+		this.zona = zona;
+	}
+	public Secao getSecao() {
+		return secao;
+	}
+	public void setSecao(Secao secao) {
+		this.secao = secao;
+	}
+	public boolean isStatus() {
+		return Status;
+	}
+	public void setStatus(boolean status) {
+		Status = status;
+	}
+	
 	
 	
 
