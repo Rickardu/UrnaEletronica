42aa55fb83a3d230dd28b202925c54bbfd70320f
Rickardu <ricarducorrea@gmail.com>
2022-03-07 19:11:05 -0300

    atualização main

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,8 +1,19 @@
 package gui;
 
+import java.util.ArrayList;
+
+import Dominio.Eleitor;
+import repositorios.EleitorRepositorio;
+import servicos.EleitorServico;
+
 public class Main {
+	public static ArrayList<Eleitor> eleitoresBR=new ArrayList<Eleitor>();
+	public static EleitorRepositorio eleitoresRepositorio=new EleitorRepositorio(eleitoresBR);
+	public static EleitorServico  eleitorServico=null;
     public static void main(String[]args){
-        System.out.println("OLA");
+    	eleitorServico=new EleitorServico(eleitoresRepositorio);
+    	 
+        
     }
     
 }
